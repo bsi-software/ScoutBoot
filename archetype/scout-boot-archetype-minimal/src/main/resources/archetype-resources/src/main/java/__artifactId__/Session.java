5812bd04f11133ac19a1cf72c6b0e40f03106c61
Jeremie Bresson <jeremie.bresson@bsi-software.com>
2017-04-19 06:31:53 +0200

    archetype: change Session super class

--- archetype/scout-boot-archetype-minimal/src/main/resources/archetype-resources/src/main/java/__artifactId__/Session.java
+++ archetype/scout-boot-archetype-minimal/src/main/resources/archetype-resources/src/main/java/__artifactId__/Session.java
@@ -5,13 +5,13 @@ package ${package}.${artifactId};
 
 import java.util.UUID;
 
-import org.eclipse.scout.boot.ui.scout.AbstractSpringBootClientSession;
+import org.eclipse.scout.boot.ui.scout.AbstractScoutBootClientSession;
 import org.eclipse.scout.rt.client.session.ClientSessionProvider;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.Bean;
 
 @Bean
-public class Session extends AbstractSpringBootClientSession {
+public class Session extends AbstractScoutBootClientSession {
 
 	public static Session get() {
 		return ClientSessionProvider.currentSession(Session.class);
